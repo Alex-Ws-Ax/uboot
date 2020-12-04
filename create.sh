49889c757f5e896e950550ee164a4f7be87746a5
ws <ws@qq.com>
2020-12-04 00:04:32 -0800

    修改create.sh脚本

--- create.sh
+++ create.sh
@@ -1,28 +1,24 @@
 #! /bin/bash
 if [ ! -f "build/u-boot.bin" ]; then
-	echo "file u-boot.bin is not exist" 
+	echo "file u-boot.bin is not exist"
 	exit 1
 fi
+filehead=262144
+filetotal=286720
 
 rm u-boot_mmc.bin
 cp build/u-boot.bin . 
 
-# #delete all file expect u-boot.bin
-# shopt -s extglob
-# rm -rf mmc_bin/*
-# shopt -u extglob
-
 cat u-boot.bin >> u-boot_mmc.bin
-# fill the temp to 256K
-while ((filelen < 262144))
+# fill the u-boot_mmc.bin to 256-KB
+while ((filelen < filehead))
 do
 	echo -n -e "\x00" >> u-boot_mmc.bin
 	filelen=`stat -c "%s" u-boot_mmc.bin`
 done
-
-while ((filelen < 286720))
+# fill the u-boot_mmc.bin to 280-KB
+while ((filelen < filetotal))
 do 
-	echo $((filelen/8192))
 	dd if=u-boot.bin of=u-boot_mmc.bin bs=1024 count=8 seek=$((filelen/1024)) conv=sync
 	filelen=`stat -c "%s" u-boot_mmc.bin`
 done
